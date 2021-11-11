99e317c178fd261dbad6496b67580b48e180d09b
Jano González <jano.gonzalez@shopify.com>
2021-11-11 14:28:09 +0100

    Support cloning into a different directory

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 home_files="editrc gemrc gitconfig inputrc tmux.conf"
-
+base_dir=$(basename $(pwd))
 mkdir -p ~/.dotfiles.old
 mkdir -p ~/.config/nvim
 
 for file in $home_files; do
   mv ~/.$file  ~/.dotfiles.old/
-  ln -s ~/.dotfiles/$file ~/.$file
+  ln -s ~/$base_dir/$file ~/.$file
 done
 
-ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/$base_dir/init.vim ~/.config/nvim/init.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
